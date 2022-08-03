07c6ff897e27c4eff54b3fa39535a9121e3bb961
Asaia Palacios <14948113+asaiapalacios@users.noreply.github.com>
2022-08-03 07:56:10 -0500

    added nav section that routes landing page to home page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,46 @@ import LeftSideComponent from "./components/LeftSideComponent"
 import MiddleChatWindow from "./components/MiddleChatWindow"
 import BottomInputComponent from "./components/BottomInputComponent"
 import RightSideComponent from "./components/RightSideComponent"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import logo from "./assets/sparrow-logo.svg"
 
 function App() {
   return (
-    <>
-      <TopNavigationBar />
-      <LeftSideComponent />
-      <MiddleChatWindow />
-      <BottomInputComponent />
-      <RightSideComponent />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <nav>
+                <img 
+                  src={logo}
+                  // temporary styling until we handle CSS styling
+                  style={{ height: 50, width: 50 }} 
+                  alt="website sparrow logo" 
+                />
+                <h1>sparrow</h1>
+                <Link to="/chat">
+                  <button>Sign In</button>
+                </Link>
+              </nav>
+            </>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <>
+              <TopNavigationBar />
+              <LeftSideComponent />
+              <MiddleChatWindow />
+              <BottomInputComponent />
+              <RightSideComponent />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
