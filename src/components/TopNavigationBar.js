16c4348a1d1629b9ea5dba5e057b5bac53a00c20
Lawrence L <Lawrence.E.Lee@gmail.com>
2022-08-06 14:40:38 -0700

    Added skeleton code for Top Navigation Bar.

--- src/components/TopNavigationBar.js
+++ src/components/TopNavigationBar.js
@@ -1,5 +1,23 @@
+import logo from '../assets/sparrow-logo.svg';
+
 function TopNavigationBar() {
-  return <div>TopNavigationBar</div>
+  return (
+		<>
+			{/* Sparrow logo is HUGE, put some temporary inline styling */}
+			<img src={logo} alt="Sparrow Logo" style={{height: "28px"}}/>
+
+			{/* Search input and button */}
+			<input type="text" placeholder='Search chat for...' />
+			<button>Search</button>
+
+			{/* Profile */}
+			<span> Placeholder Username </span>
+
+			{/* Logout button */}
+			<button>Logout</button>
+
+		</>
+	)
 }
 
 export default TopNavigationBar
