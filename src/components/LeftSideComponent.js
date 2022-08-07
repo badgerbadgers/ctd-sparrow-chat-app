77337d87c9d4dc209788c249024b5619bd827647
Vinicius Pietscher <vini.914@gmail.com>
2022-08-07 19:19:46 -0400

    fetch users list from firestore

--- src/components/LeftSideComponent.js
+++ src/components/LeftSideComponent.js
@@ -1,33 +1,25 @@
-// import { db } from "../config/fire-config"
-// import { collection, getDocs } from "firebase/firestore"
-// import { useState, useEffect } from "react"
-
-const userList = [
-  { id: 0, name: "user0" },
-  { id: 1, name: "user1" },
-  { id: 2, name: "user2" },
-  { id: 3, name: "user3" },
-  { id: 4, name: "user4" },
-  { id: 5, name: "user5" },
-]
+import { db } from "../config/fire-config"
+import { collection, getDocs } from "firebase/firestore"
+import { useState, useEffect } from "react"
 
 function LeftSideComponent() {
-  // const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([])
 
-  // const usersCollectionRef = collection(db, "users")
+  const usersCollectionRef = collection(db, "users")
 
-  // useEffect(() => {
-  //   const getUsers = async () => {
-  //     const data = await getDocs(usersCollectionRef)
-  //     setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
-  //   }
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef)
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    }
 
-  //   getUsers()
-  // }, [])
+    getUsers()
+  }, [])
+
   return (
     <>
       <ul>
-        {userList.map((user) => {
+        {users.map((user) => {
           return <li key={user.id}>{user.name}</li>
         })}
       </ul>
