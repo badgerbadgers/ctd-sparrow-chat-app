53ba0cb995404b434c957196d5e43cb7aac64fc6
Tim Almacen <tim.almacen@Tims-Air.attlocal.net>
2022-08-08 14:19:59 -0700

    index.js has browser router wrapped around App.js component, commented out searchbar not part of mvp, in App.js RightSideComponent is commented out not part of mvp, there are now routes for signin and chat, the chat routes to the main chat window that includes four components that are persists until user is signed out of app.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,47 @@
-import TopNavigationBar from "./components/TopNavigationBar"
-import LeftSideComponent from "./components/LeftSideComponent"
-import MiddleChatWindow from "./components/MiddleChatWindow"
-import BottomInputComponent from "./components/BottomInputComponent"
+import TopNavigationBar from "./components/TopNavigationBar";
+import LeftSideComponent from "./components/LeftSideComponent";
+import MiddleChatWindow from "./components/MiddleChatWindow";
+import BottomInputComponent from "./components/BottomInputComponent";
 import RightSideComponent from "./components/RightSideComponent"
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import logo from "./assets/sparrow-logo.svg"
-
+import { Routes, Route, Link } from "react-router-dom";
+import logo from "./assets/sparrow-logo.svg";
+ 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div>
-                <img 
-                  src={logo}
-                  // temporary inline styling until we handle CSS
-                  style={{ height: 75, width: 75 }} 
-                  alt="Sparrow Logo" 
-                />
-                <h3>sparrow</h3>
-                <Link to="/chat">
-                  <button>Sign in with G</button>
-                </Link>
-              </div>
-            </>
-          }
-        />
-        <Route
-          path="/chat"
-          element={
-            <>
-              <TopNavigationBar />
-              <LeftSideComponent />
-              <MiddleChatWindow />
-              <BottomInputComponent />
-              <RightSideComponent />
-            </>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  )
+ return (
+   <Routes>
+     <Route path="/signin"
+       element={
+         <>
+           <div>
+             <img
+               src={logo}
+               // temporary inline styling until we handle CSS
+               style={{ height: 75, width: 75 }}
+               alt={"Sparrow Logo"}
+             />
+             <h3>sparrow</h3>
+             <Link to="/signin">
+               <button>Sign in with G</button>
+             </Link>
+           </div>
+         </>
+       }
+     />
+     <Route path="/chat"
+       element={
+          <>
+           <TopNavigationBar />
+           <LeftSideComponent />
+           <MiddleChatWindow />
+           <BottomInputComponent />
+           <RightSideComponent />
+          </>
+       }
+     />
+   </Routes>
+ );
 }
+ 
+export default App;
+ 
 
-export default App
