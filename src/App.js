fce26b0ca2a867ccc898bd516750f7584f8eb208
Asaia Palacios <14948113+asaiapalacios@users.noreply.github.com>
2022-08-08 18:52:34 -0500

    authenticate user when signing in (new version)

--- src/App.js
+++ src/App.js
@@ -1,70 +1,46 @@
-import React from "react"
-// Firebase SDK
-import firebase from "firebase/compat/app"
-import "firebase/compat/auth" 
-// Hooks
-import { useAuthState } from "react-firebase-hooks/auth"
-
-import app from "./config/fire-config"
-import logo from "./assets/sparrow-logo.svg"
-
-import TopNavigationBar from "./components/TopNavigationBar"
-import LeftSideComponent from "./components/LeftSideComponent"
-import MiddleChatWindow from "./components/MiddleChatWindow"
-import BottomInputComponent from "./components/BottomInputComponent"
+import { signInWithGoogle } from "./config/fire-config";
+import TopNavigationBar from "./components/TopNavigationBar";
+import LeftSideComponent from "./components/LeftSideComponent";
+import MiddleChatWindow from "./components/MiddleChatWindow";
+import BottomInputComponent from "./components/BottomInputComponent";
 import RightSideComponent from "./components/RightSideComponent"
-
-// Import our initialized Firebase app (to identify our project)
-// Make a reference to auth SDK as a global variable
-const auth = app.auth()
-
+import { Routes, Route, Link } from "react-router-dom";
+import logo from "./assets/sparrow-logo.svg";
+ 
 function App() {
-  // Identify whether or not a user is logged in
-  const [user] = useAuthState(auth)
-
-  return (
-    <div>
-      <section>
-        {/* If the user is defined, show the chat room; otherwise, show the sign-in page */}
-        {user ? <Chat /> : <SignIn />}
-      </section>
-    </div>
-  )
+ return (
+   <Routes>
+     <Route path="/signin"
+       element={
+         <>
+           <div>
+             <img
+               src={logo}
+               // temporary inline styling until we handle CSS
+               style={{ height: 75, width: 75 }}
+               alt={"Sparrow Logo"}
+             />
+             <h3>sparrow</h3>
+             <Link to="/chat">
+               <button onClick={signInWithGoogle}>Sign in with G</button>
+             </Link>
+           </div>
+         </>
+       }
+     />
+     <Route path="/chat"
+       element={
+          <>
+           <TopNavigationBar />
+           <LeftSideComponent />
+           <MiddleChatWindow />
+           <BottomInputComponent />
+           <RightSideComponent />
+          </>
+       }
+     />
+   </Routes>
+ );
 }
-
-function SignIn() {
-  const signInWithGoogle = () => {
-    // Instantiate Google auth provider
-    const provider = new firebase.auth.GoogleAuthProvider()
-    // Trigger pop-up window to authenticate user when user clicks the sign-in button
-    auth.signInWithPopup(provider)
-  }
-  
-  return (
-    <div>
-      <img 
-        src={logo}
-        // Temporary inline styling until we handle CSS
-        style={{ height: 75, width: 75 }} 
-        alt="Sparrow Logo" 
-      />
-      <h3>sparrow</h3>
-      {/* Button listens to the click event and runs the signIn function */}
-      <button onClick={signInWithGoogle}>Sign in with G</button>
-    </div>
-  )
-}
-
-function Chat() {
-  return (
-    <div>
-      <TopNavigationBar />
-      <LeftSideComponent />
-      <MiddleChatWindow />
-      <BottomInputComponent />
-      <RightSideComponent />
-    </div>
-  )
-}
-
-export default App
+ 
+export default App;
--- src/config/fire-config.js
+++ src/config/fire-config.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app"
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,7 +16,17 @@ const firebaseConfig = {
 }
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 
-export const db = getFirestore(app)
-export default app
+export const db = getFirestore(app);
+
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, provider).then((result) => {
+    console.log(result);
+  }).catch((error) => {
+    console.log(error);
+  })
+};
