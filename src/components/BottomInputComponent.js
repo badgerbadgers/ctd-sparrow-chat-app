62cc3e2e7e08faccf2993a97b51972f5079cfa0e
Vinicius Pietscher <vini.914@gmail.com>
2022-08-09 15:17:36 -0400

    post messages to firestore

--- src/components/BottomInputComponent.js
+++ src/components/BottomInputComponent.js
@@ -1,14 +1,49 @@
+import { useState } from "react"
+import { db } from "../config/fire-config"
+import { collection, addDoc, serverTimestamp } from "firebase/firestore"
+
 function BottomInputComponent() {
+  const [message, setMessage] = useState("")
+
+  const handleMessageChange = (e) => {
+    const newMessage = e.target.value
+    setMessage(newMessage)
+  }
+
+  const messagesCollectionRef = collection(db, "messages")
+
+  // Saves a new message to Cloud Firestore.
+  async function saveMessage(messageText) {
+    // Add a new message entry to the Firebase database.
+    try {
+      await addDoc(messagesCollectionRef, {
+        name: "getUserName",
+        text: messageText,
+        profilePicUrl: "getProfilePicUrl",
+        timestamp: serverTimestamp(),
+      })
+    } catch (error) {
+      console.error("Error writing new message to Firebase Database", error)
+    }
+  }
+
+  const handleSubmitMessage = (e) => {
+    e.preventDefault()
+    saveMessage(message)
+    setMessage("")
+  }
+
   return (
-    <div>
-      <input 
-        type="text"
+    <form onSubmit={handleSubmitMessage}>
+      <input
+        type='text'
+        value={message}
         placeholder='type something'
-        >
-        </input>
+        onChange={handleMessageChange}
+      ></input>
       <button>Submit</button>
-    </div>
-    )
+    </form>
+  )
 }
 
 export default BottomInputComponent
