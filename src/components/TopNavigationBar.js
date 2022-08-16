d6f0284f6412f527396499c4cfffd7164d56de4e
Tim Almacen <tim.almacen@Tims-MacBook-Air.local>
2022-08-15 23:08:33 -0700

    created bootstrap navbar component, added sparrow logo, displays username in navbar and signout button with logic all included in navbar

--- src/components/TopNavigationBar.js
+++ src/components/TopNavigationBar.js
@@ -1,30 +1,39 @@
-import logo from "../assets/sparrow-logo.svg"
+import logo from "../assets/sparrow-logo-w-bg.png"
 import { signOutUser, removeUser } from "../config/googleSignIn"
 import { Link } from "react-router-dom"
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import Button from "react-bootstrap/Button"
+import Image from "react-bootstrap/Image"
 
 function TopNavigationBar({ currentUser }) {
+  console.log(currentUser)
+  const signedInUser = currentUser ? currentUser.displayName : "user"
+  // const profilePic = currentUser ? currentUser.photoUrl : null
+
   // When signing out removes user from 'users' collection and signs out user.
   const handleSignOutUser = () => {
-    removeUser(currentUser)
+    // removeUser(currentUser)
     signOutUser()
   }
   return (
-    <>
-      {/* Sparrow logo is HUGE, put some temporary inline styling */}
-      <img src={logo} alt='Sparrow Logo' style={{ height: "28px" }} />
-
-      {/* Search input and button */}
-      {/* <input type="text" placeholder='Search chat for...' /> */}
-      {/* <button>Search</button> */}
-
-      {/* Profile */}
-      <span> Placeholder Username </span>
-
-      {/* Logout button */}
-      <Link to='/'>
-        <button onClick={handleSignOutUser}>Logout</button>
-      </Link>
-    </>
+    <Navbar expand='lg'>
+      <Container>
+        <Image src={logo} width='75' />
+        {/* <img className='navbar-brand img-thumbnail' src={logo} alt='logo'></img> */}
+        <p className='navbar-text'>{signedInUser}</p>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            {/* <p className='navbar-text'>{signedInUser}</p> */}
+            <Link to='/'>
+              <Button onClick={handleSignOutUser}>Sign Out</Button>
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
