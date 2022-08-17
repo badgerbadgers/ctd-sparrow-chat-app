ff9918e448f34909db2dd4a32c23aba391ba1d2a
Tim Almacen <tim.almacen@Tims-MacBook-Air.local>
2022-08-17 11:57:10 -0700

    TopNavigationBar component now includes style branding including logo, background color and button color

--- src/components/TopNavigationBar.js
+++ src/components/TopNavigationBar.js
@@ -2,38 +2,34 @@ import logo from "../assets/sparrow-logo-w-bg.png"
 import { signOutUser, removeUser } from "../config/googleSignIn"
 import { Link } from "react-router-dom"
 import Container from "react-bootstrap/Container"
-import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 import Button from "react-bootstrap/Button"
 import Image from "react-bootstrap/Image"
+import style from "./TopNavigationBar.module.css"
 
 function TopNavigationBar({ currentUser }) {
-  console.log(currentUser)
   const signedInUser = currentUser ? currentUser.displayName : "user"
-  // const profilePic = currentUser ? currentUser.photoUrl : null
 
   // When signing out removes user from 'users' collection and signs out user.
   const handleSignOutUser = () => {
-    // removeUser(currentUser)
+    removeUser(currentUser)
     signOutUser()
   }
   return (
-    <Navbar expand='lg'>
-      <Container>
-        <Image src={logo} width='75' />
-        {/* <img className='navbar-brand img-thumbnail' src={logo} alt='logo'></img> */}
-        <p className='navbar-text'>{signedInUser}</p>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='me-auto'>
-            {/* <p className='navbar-text'>{signedInUser}</p> */}
-            <Link to='/'>
-              <Button onClick={handleSignOutUser}>Sign Out</Button>
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar className={style.navbarContainer}>
+        <Container>
+          <Image src={logo} width='75' />
+          <p className={style.navText}>{signedInUser}</p>
+          <Navbar.Toggle aria-expanded='true' />
+          <Link to='/'>
+            <Button className={style.navButton} onClick={handleSignOutUser}>
+              Sign Out
+            </Button>
+          </Link>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
