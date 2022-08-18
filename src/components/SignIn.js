7db22514b6c96c2620d42e786c3135588c0d4b8d
Asaia Palacios <14948113+asaiapalacios@users.noreply.github.com>
2022-08-18 10:54:46 -0500

    added color using custom color file for login page (#84)

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,6 +4,7 @@ import { onAuthStateChanged } from "firebase/auth"
 import logo from "../assets/sparrow-logo.svg"
 import { Link } from "react-router-dom"
 import Button from "react-bootstrap/Button"
+import style from "./SignIn.module.css"
 
 function SignIn({ handleAuthStateChange }) {
   useEffect(() => {
@@ -13,20 +14,27 @@ function SignIn({ handleAuthStateChange }) {
   }, [])
 
   return (
-    <div className='min-vh-100 d-flex justify-content-center align-items-center flex-column border border-dark mx-auto w-50'>
-      <img src={logo} style={{ height: 75, width: 75 }} alt='Sparrow Logo' />
-      <h1 className='h6'>sparrow</h1>
-      <Link to='/chat'>
-        <div className='mt-5'>
-          <Button
-            variant='light'
-            onClick={signInWithGoogle}
-            className='px-4 rounded-pill'
-          >
-            Sign in with <i className='bi bi-google'></i>
-          </Button>
-        </div>
-      </Link>
+    <div className='bg-secondary'>
+      <div className='min-vh-100 d-flex justify-content-center align-items-center flex-column border border-dark mx-auto w-50 bg-primary'>
+        <img
+          src={logo}
+          className={style.filterYellow}
+          style={{ height: 75, width: 75 }}
+          alt='Sparrow Logo'
+        />
+        <h1 className='h6 text-warning'>sparrow</h1>
+        <Link to='/chat'>
+          <div className='mt-5'>
+            <Button
+              variant='light'
+              onClick={signInWithGoogle}
+              className='px-4 rounded-pill bg-info'
+            >
+              Sign in with <i className='bi bi-google'></i>
+            </Button>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
