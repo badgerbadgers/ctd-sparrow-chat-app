f96099d94b5e0cb36dbc1ad2ca4d69cc90041d1b
Tim Almacen <tim.almacen@Tims-Air.attlocal.net>
2022-08-18 16:19:24 -0700

    LeftSideComponent now has a left side bar created from bootstrap code, styled following the brand guide.

--- src/components/LeftSideComponent.js
+++ src/components/LeftSideComponent.js
@@ -7,11 +7,15 @@ import {
   limit,
 } from "firebase/firestore"
 import { useState, useEffect } from "react"
-import { Nav } from "react-bootstrap"
 import style from "./LeftSideComponent.module.css"
+import Offcanvas from "react-bootstrap/Offcanvas"
+import Button from "react-bootstrap/Button"
 
 // Component renders list on currently logged in users in application
-function LeftSideComponent() {
+function LeftSideComponent({ name, ...props }) {
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
   const [users, setUsers] = useState([])
   const usersCollectionRef = collection(db, "users")
   const queryUsers = query(usersCollectionRef, orderBy("name"), limit(20))
@@ -27,43 +31,40 @@ function LeftSideComponent() {
   }, [])
 
   return (
-    <>
-      <h4>Logged in users: {users.length}</h4>
-      {users.length > 0 ? (
-        <ul class={style.listGroup}>
-          {users.map((user) => {
-            return (
-              <li key={user.id} className={style.listGroupItem}>
-                {user.name}
-              </li>
-            )
-          })}
-        </ul>
-      ) : (
-        <p>No user online</p>
-      )}
-      <Nav
-        className='col-md-12 d-none d-md-block bg-light sidebar'
-        activeKey='/home'
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    <div className='bg-secondary text-light'>
+      <Button onClick={handleShow} className='me-2' variant='secondary'>
+        Users
+      </Button>
+      <Offcanvas
+        show={show}
+        onHide={handleClose}
+        {...props}
+        className='text-light'
       >
-        <div className='sidebar-sticky'></div>
-        <Nav.Item>
-          <Nav.Link href='/home'>Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey='link-1'>Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey='link-2'>Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey='disabled' disabled>
-            Disabled
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </>
+        <Offcanvas.Header closeButton className='bg-secondary'>
+          <Offcanvas.Title className='bg-secondary'>
+            {" "}
+            Users: {users.length}
+          </Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body className='bg-secondary'>
+          <h4>Logged in users: {users.length}</h4>
+          {users.length > 0 ? (
+            <ul class={style.listGroup}>
+              {users.map((user) => {
+                return (
+                  <li key={user.id} className={style.listGroupItem}>
+                    {user.name}
+                  </li>
+                )
+              })}
+            </ul>
+          ) : (
+            <p>No user online</p>
+          )}
+        </Offcanvas.Body>
+      </Offcanvas>
+    </div>
   )
 }
 
