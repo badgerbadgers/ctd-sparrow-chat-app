11f40be42202de6005dc0a81746e4867504c0bbc
Lawrence L <Lawrence.E.Lee@gmail.com>
2022-08-21 15:26:17 -0700

    Compeleted conditional render of spinner image, when in the middle of sigin process. After signin process completes, will route to /chat.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import LeftSideComponent from "./components/LeftSideComponent"
 import MiddleChatWindow from "./components/MiddleChatWindow"
 import BottomInputComponent from "./components/BottomInputComponent"
 import SignIn from "./components/SignIn"
-import { Routes, Route } from "react-router-dom"
-import { useState } from "react"
+import { Routes, Route, useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 function App() {
   const [authState, setAuthState] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+	const navigate = useNavigate();
 
   const handleAuthStateChange = (auth) => {
     setAuthState(auth);
@@ -18,6 +19,13 @@ function App() {
 		setIsLoading(!isLoading);
 		setTimeout(1000);
 	}
+
+	useEffect(() => {
+		if (authState) {
+			setIsLoading(false);
+			navigate("/chat");
+		}
+	}, [authState]);
 
   return (
     <>
@@ -40,7 +48,9 @@ function App() {
           path='/chat'
           element={
             <>
-      				<TopNavigationBar currentUser={authState} /> <LeftSideComponent /> <MiddleChatWindow currentUser={authState} />
+      				<TopNavigationBar currentUser={authState} /> 
+							<LeftSideComponent /> 
+							<MiddleChatWindow currentUser={authState} />
               <BottomInputComponent currentUser={authState}/>
             </>
           }
