1a74c25f124496b5d5fe1dcb3b9b49ea3a07d374
Vinicius Pietscher <vini.914@gmail.com>
2022-08-22 08:57:55 -0400

    navigate user to '/' if user is not authenticated

---
+++ src/components/Chat.js
@@ -0,0 +1,27 @@
+import TopNavigationBar from "./TopNavigationBar"
+import LeftSideComponent from "./LeftSideComponent"
+import MiddleChatWindow from "./MiddleChatWindow"
+import BottomInputComponent from "./BottomInputComponent"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
+function Chat({ currentUser }) {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!currentUser) {
+      navigate("/")
+    }
+  }, [])
+
+  return (
+    <>
+      <TopNavigationBar currentUser={currentUser} />
+      <LeftSideComponent />
+      <MiddleChatWindow currentUser={currentUser} />
+      <BottomInputComponent currentUser={currentUser} />
+    </>
+  )
+}
+
+export default Chat
