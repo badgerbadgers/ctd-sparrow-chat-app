957846c21da0eb4baef3f6a8a30360a27798b2a6
Asaia Palacios <14948113+asaiapalacios@users.noreply.github.com>
2022-08-24 13:36:21 -0500

    styled navbar using inline styling w/bootstrap-react & bootstrap (#117)

--- src/components/TopNavigationBar.js
+++ src/components/TopNavigationBar.js
@@ -1,11 +1,10 @@
-import logo from "../assets/sparrow-navbar-w-bg-logo.png"
+import logo from "../assets/sparrow-logo.svg"
 import { signOutUser, removeUser } from "../config/googleSignIn"
 import { Link } from "react-router-dom"
 import Container from "react-bootstrap/Container"
 import Navbar from "react-bootstrap/Navbar"
 import Button from "react-bootstrap/Button"
 import Image from "react-bootstrap/Image"
-import Col from "react-bootstrap/Col"
 import style from "./TopNavigationBar.module.css"
 
 function TopNavigationBar({ currentUser }) {
@@ -15,37 +14,40 @@ function TopNavigationBar({ currentUser }) {
     signOutUser()
   }
   return (
-    <>
-      <Navbar className={style.navbarContainer}>
+    <div>
+      <Navbar bg='primary navbar-dark' className='pt-3'>
         <Container>
-          <Col md={7}>
-            <Image src={logo} width='70' />
-          </Col>
-          <Col sm={1}>
-            <Image
-              src={currentUser ? currentUser.photoURL : ""}
+          <Navbar.Brand>
+            <img
+              src={logo}
               width='50'
-              className={style.logoImg}
+              height='50'
+              className={style.filterYellow}
+              alt='Sparrow Logo'
             />
-          </Col>
-          <Col sm={2}>
-            <p className={style.navText}>
-              {currentUser ? currentUser.displayName : ""}
-            </p>
-          </Col>
-          <Col sm={2}>
-            <Link to='/'>
-              <Button className={style.navButton} onClick={handleSignOutUser}>
-                Sign Out
-              </Button>
-            </Link>
-          </Col>
+            <p className='h6 text-warning'>sparrow</p>
+          </Navbar.Brand>
+          <span className='fs-5 text-white ms-auto me-2'>
+            {currentUser ? currentUser.displayName : ""}
+          </span>
+          <Image
+            src={currentUser ? currentUser.photoURL : ""}
+            width='55'
+            height='55'
+            className='me-5 rounded-circle'
+          />
+          <Link to='/'>
+            <Button
+              variant='light'
+              className='px-3 rounded bg-info'
+              onClick={handleSignOutUser}
+            >
+              Sign Out
+            </Button>
+          </Link>
         </Container>
       </Navbar>
-<section className={style.Channels}>
-  Channel: Main group
-</section>
-    </>
+    </div>
   )
 }
 
