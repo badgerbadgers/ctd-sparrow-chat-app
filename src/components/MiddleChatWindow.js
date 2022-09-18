a887d909229e3eb3eef208638636652d713030cb
Tim <tim.almacen@gmail.com>
2022-09-18 08:48:52 -0700

    else if statement MiddleChatWindow is now similar to previous logic where last name gets split and a period gets added.

--- src/components/MiddleChatWindow.js
+++ src/components/MiddleChatWindow.js
@@ -24,7 +24,7 @@ function MiddleChatWindow({ currentUser, messages, screenBottom }) {
             //if username contains a space then format second text with period if there is more than one character or just return the second character with no period
           } else if (message.name.includes(" ")) {
             name = `${message.name.split(" ")[0]} ${
-              message.name.split(" ")[1]
+              message.name.split(" ")[1][0]
             }.`
             //by default return user name
           } else {
