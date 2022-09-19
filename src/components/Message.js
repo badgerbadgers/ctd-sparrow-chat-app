fb2709c714d92c150ffde1e65e5845c653ff247e
LongJourney47 <fernandezjw07@gmail.com>
2022-09-19 17:55:52 -0400

    styles: refactored message box in middle chat window.

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,16 @@
-import { useMemo } from "react"
+import { useMemo, useContext } from "react"
+
 import TimeAgo from "javascript-time-ago"
 import en from "javascript-time-ago/locale/en"
 import "./Message.css"
+import UserLogo from "../assets/sparrow-user-profile.svg"
+import { ThemeContext } from "../themeContext.js"
 
 TimeAgo.addLocale(en)
 
 function Message({ name, text, timestamp, email, profilePicUrl }) {
+  const { theme } = useContext(ThemeContext)
+
   const timeAgo = useMemo(
     () => new TimeAgo("en-US").format(timestamp.toDate()),
     [timestamp]
@@ -13,13 +18,63 @@ function Message({ name, text, timestamp, email, profilePicUrl }) {
   const username = name.includes("@")
     ? email
     : `${name.split(" ")[0]} ${name.split(" ")[1][0]}.`
+
+
+
+
   return (
-    <>
-      <br />
-      <p>{timeAgo}</p>
-      <p>{text}</p>
-      <p>{username}</p>
-    </>
+
+
+    <div 
+    className={
+            text.email === profilePicUrl.email
+              ? "profile-pic-and-message-end"
+              : "profile-pic-and-message"
+          }
+          style={{ backgroundColor: theme.backgroundColor }}
+    
+    >
+
+      
+   <img
+                className='user-image'
+                // Conditional statement for profile image
+                src={profilePicUrl ? profilePicUrl : UserLogo}
+                width='75'
+                alt='profile pic'
+            />
+
+
+
+
+
+
+
+    <li className='list-item'>
+
+ 
+
+
+          
+     
+      <div className='card-header d-flex justify-content-between p-0 list-item-divider'>
+      <p className='list-item-name fw-bold mb-0'>{username}</p>
+        </div>
+
+        <div className='card-body'>
+                <p className='mb-0'>{text}</p>
+          </div>
+
+
+          <div className='card-header d-flex justify-content-end p-0'>
+                  <p className='text-dark small mb-0 mt-1'>{timeAgo}</p>
+               </div>
+
+  
+     
+    </li>
+
+    </div>
   )
 }
 
--- src/components/MiddleChatWindow.js
+++ src/components/MiddleChatWindow.js
@@ -2,9 +2,9 @@ import { useContext } from "react"
 import "./MiddleChatWindow.css"
 import { ThemeContext } from "../themeContext.js"
 import Message from "./Message"
-import UserLogo from "../assets/sparrow-user-profile.svg"
 
-function MiddleChatWindow({ currentUser, messages, screenBottom }) {
+
+function MiddleChatWindow({ messages, screenBottom }) {
   const { theme } = useContext(ThemeContext)
 
   return (
@@ -17,41 +17,12 @@ function MiddleChatWindow({ currentUser, messages, screenBottom }) {
         <div ref={screenBottom}></div>
         {/* renders message */}
         {messages.map((message) => {
-          // const name = message.name.includes("@")
+         
           //   ? message.email
-          //   : `${message.name.split(" ")[0]} ${message.name.split(" ")[1][0]}.`
+     
 
           return <Message key={message.id} {...message} />
-          // return (
-          //   <div
-          //     key={message.id}
-          //     className={
-          //       message.email === currentUser.email
-          //         ? "profile-pic-and-message-end"
-          //         : "profile-pic-and-message"
-          //     }
-          //     style={{ backgroundColor: theme.backgroundColor }}
-          //   >
-          //     <img
-          //       className='user-image'
-          //       // Conditional statement for profile image
-          //       src={message.profilePicUrl ? message.profilePicUrl : UserLogo}
-          //       width='75'
-          //       alt='profile pic'
-          //     />
-          //     <li className='list-item'>
-          //       <div className='card-header d-flex justify-content-between p-0 list-item-divider'>
-          //         <p className='list-item-name fw-bold mb-0'>{name}</p>
-          //       </div>
-          //       <div className='card-body'>
-          //         <p className='mb-0'>{message.text}</p>
-          //       </div>
-          //       <div className='card-header d-flex justify-content-end p-0'>
-          //         <p className='text-dark small mb-0 mt-1'>1 min ago</p>
-          //       </div>
-          //     </li>
-          //   </div>
-          // )
+       
         })}
       </ul>
     </>
